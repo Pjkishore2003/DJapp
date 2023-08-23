ba7fe3bfa34b6a47bad7389dc0b1526d1fe94bca
Pjkishore2003 <142987411+Pjkishore2003@users.noreply.github.com>
2023-08-23 23:39:38 +0530

    Update settings.py

--- project/project/settings.py
+++ project/project/settings.py
@@ -121,4 +121,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
- ALLOWED_HOSTS = ['Public IP of EC2', 'localhost', '0.0.0.0']
+ #ALLOWED_HOSTS = ['Public IP of EC2', 'localhost', '0.0.0.0']
